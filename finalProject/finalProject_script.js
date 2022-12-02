4dcf2b04eb2f19701b0f494598ad51086b293696
Flora Huang <99567644+fhuan10@users.noreply.github.com>
2022-12-02 20:58:23 +0000

    Updated the homepage and the article search page

--- finalProject/finalProject_script.js
+++ finalProject/finalProject_script.js
@@ -1,21 +1,21 @@
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new mdc.topAppBar.MDCTopAppBar(topAppBarElement);
 
-const drawer = new mdc.drawer.MDCDrawer(document.querySelector('.mdc-drawer'));
+// const drawer = new mdc.drawer.MDCDrawer(document.querySelector('.mdc-drawer'));
 
 // document.querySelectorAll("a").forEach(item => {
 //   console.log(item);
 // })
 
-// Switch screens
-document.querySelectorAll("a").forEach(item => {
+// Switch screens (TEMP)
+document.querySelectorAll(".div-screen").forEach(item => {
   item.addEventListener("click", (event) => {
     event.preventDefault();
 
     let screen = event.target.getAttribute("data-screen");
     console.log(screen);
 
-    document.querySelectorAll("div.screen").forEach(screen => screen.style.display = "none")
+    document.querySelectorAll("main.screen").forEach(screen => screen.style.display = "none")
 
     document.querySelector("#" + screen).style.display = "block";
   })
