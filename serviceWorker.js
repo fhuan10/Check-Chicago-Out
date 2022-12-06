eb589046b0d000b16ee2e266e43feeae96cca9e1
Flora Huang <99567644+fhuan10@users.noreply.github.com>
2022-12-06 21:20:32 +0000

    Updated the manifest file Added the service worker file Deleted untitled projects

---
+++ serviceWorker.js
@@ -0,0 +1,36 @@
+const staticChicagoApp = "check-chicago-app-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "Final/index.html",
+  "Final/chicagoArticles.js",
+  "Final/database.js",
+  "Final/displayFilter.js",
+  "Final/final-style.css",
+  "Final/final.js",
+  "Final/map.js",
+  "Final/images/chicagoView.jpeg",
+  "Final/images/theBean.jpg",
+  "Final/images/chicago-tower.png",
+  "NavigationDrawer/drawer.js",
+  "NavigationDrawer/index.html",
+  "NavigationDrawer/style2.css"
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticChicagoApp).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
